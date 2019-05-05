e74d9b08666aa9561ed76efe5da734ecc4b09e5f
PotatoTax <conor@johngregg.org>
2019-05-05 14:39:54 -0500

    Completed activity viewer, nav
    Signed-off-by: PotatoTax <conor@johngregg.org>

--- HomePage.py
+++ HomePage.py
@@ -169,15 +169,54 @@ class HomeRight(GridLayout):
         super().__init__(**kwargs)
         self.main_app = main_app
         self.cols = 1
-        self.add_widget(ActivityWidgets(0, self.main_app))
-        self.add_widget(ActivityWidgets(1, self.main_app))
-        self.add_widget(ActivityWidgets(2, self.main_app))
-        self.add_widget(ActivityWidgets(3, self.main_app))
+        self.page = 0
+
+        self.navigation = GridLayout(cols=4, rows=1, size_hint_y=.05)
+
+        self.navigation.add_widget(Label())
+
+        self.navigation.forward = Button(text="Forward", size_hint_x=.4)
+        self.navigation.forward.bind(on_press=lambda e: self.page_forward())
+        self.navigation.add_widget(self.navigation.forward)
+
+        self.navigation.back = Button(text="Back", size_hint_x=.4)
+        self.navigation.back.bind(on_press=lambda e: self.page_back())
+        self.navigation.add_widget(self.navigation.back)
+
+        self.navigation.add_widget(Label())
+
+        self.add_widget(self.navigation)
+
+        self.activity_viewer = GridLayout(cols=1, rows=4, size_hint_y=.95)
+        self.add_widget(self.activity_viewer)
+        self.show_activities()
+
+    def page_forward(self):
+        if self.page > 0:
+            self.page -= 1
+            print("Forward")
+            self.show_activities()
+
+    def page_back(self):
+        if self.page * 4 < len(self.main_app.activity_data.activity_list):
+            self.page += 1
+            print("Back")
+            self.show_activities()
+
+    def show_activities(self):
+        self.activity_viewer.clear_widgets()
+        for i in range(4):
+            try:
+                self.activity_viewer.add_widget(ActivityWidgets(i + self.page * 4, self.main_app, size_hint_y=.25))
+            except IndexError:
+                print("No activity with index %s".format(i + self.page * 4))
 
 
 class ActivityWidgets(GridLayout):
     def __init__(self, num, main_app, **kwargs):
         super().__init__(**kwargs)
+        self.months = {1: 'January', 2: 'Febuary', 3: 'March', 4: 'April', 5: 'May',
+                       6: 'June', 7: 'July', 8: 'August', 9: 'September', 10: 'October', 11: 'November', 12: 'December'}
         self.main_app = main_app
         self.data = self.main_app.activity_data.activity_list[num]
         self.cols = 1
@@ -185,8 +224,11 @@ class ActivityWidgets(GridLayout):
 
         self.user_date = GridLayout()
         self.user_date.cols = 1
-        self.user_date.add_widget(Label(text=self.main_app.user_data['Username']))
-        self.user_date.add_widget(Label(text=self.data["Date"]))
+        date = self.data["Date"][:19]
+        year = date[:4]
+        month = self.months[int(date[5:7])]
+        day = int(date[8:10])
+        self.user_date.add_widget(Label(text=(month + " " + str(day) + ', ' + year)))
         self.add_widget(self.user_date)
 
         self.name_type = GridLayout()
@@ -201,19 +243,34 @@ class ActivityWidgets(GridLayout):
         self.activity_data.distance = GridLayout()
         self.activity_data.distance.cols = 1
         self.activity_data.distance.add_widget(Label(text="Distance"))
-        self.activity_data.distance.add_widget(Label(text=str(self.data["Distance"])))
+        self.activity_data.distance.add_widget(Label(text=str(self.data["Distance"]) + " mi"))
         self.activity_data.add_widget(self.activity_data.distance)
 
         self.activity_data.pace = GridLayout()
         self.activity_data.pace.cols = 1
         self.activity_data.pace.add_widget(Label(text="Pace"))
-        self.activity_data.pace.add_widget(Label(text="7:00 /mi"))
+        pace = (self.data["Time"] / 60) / self.data["Distance"]
+        minutes = int(pace)
+        seconds = int(pace % 1 * 60)
+        if seconds < 10:
+            seconds = "0" + str(seconds)
+        self.activity_data.pace.add_widget(Label(text=str(minutes) + ":" + str(seconds) + " /mi"))
         self.activity_data.add_widget(self.activity_data.pace)
 
         self.activity_data.time = GridLayout()
         self.activity_data.time.cols = 1
         self.activity_data.time.add_widget(Label(text="Time"))
-        self.activity_data.time.add_widget(Label(text=str(self.data["Time"])))
+        time = self.data["Time"] / 60
+        if time > 60:
+            hours = int(time / 60)
+            minutes = int(time - hours * 60)
+
+            time = str(hours) + "h " + str(minutes) + "m"
+        else:
+            minutes = int(time)
+            seconds = int(time % 1 * 60)
+            time = str(minutes) + "m " + str(seconds) + "s"
+        self.activity_data.time.add_widget(Label(text=str(time)))
         self.activity_data.add_widget(self.activity_data.time)
 
         self.add_widget(self.activity_data)
