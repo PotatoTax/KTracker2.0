efeb12290a787248d4795643052c6430cabef810
PotatoTax <conor@johngregg.org>
2019-05-06 20:37:04 -0500

    Completed searching
    Signed-off-by: PotatoTax <conor@johngregg.org>

--- MyActivitiesPage.py
+++ MyActivitiesPage.py
@@ -1,13 +1,8 @@
 from kivy.uix.button import Button
-from kivy.uix.dropdown import DropDown
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.spinner import Spinner
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.textinput import TextInput
 
 from HeaderWidget import Header
 
